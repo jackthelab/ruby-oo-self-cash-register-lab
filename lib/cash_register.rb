4fe05d83e4401013883b24d85e1077e37f027b9d
Jacob Hampton <jhampton792@gmail.com>
2021-01-12 22:49:08 -0600

    commit tree 5800a01a1394babbe64181401de250003bf677c5

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+class CashRegister
 
+    def initialize(employee_discount=0)
+        @total = 0
+        @discount = employee_discount
+        @items = []
+        @prices = []
+    end
+
+    attr_reader :discount, :items
+
+    attr_accessor :total
+
+    def add_item(item, price, quantity=1)
+        @total += (price * quantity)
+        quantity.times do
+            items.push(item)
+        end
+        @prices.push(price * quantity)
+    end
+
+    def apply_discount
+        if @discount != 0
+            @total *= ((100 - @discount) * 0.01)
+            @total = @total.to_i
+            return "After the discount, the total comes to $#{@total}."
+        else
+            return "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        @item_to_remove = @items.pop
+        @price_to_remove = @prices.pop
+        @total -= @price_to_remove
+    end
+
+
+end
